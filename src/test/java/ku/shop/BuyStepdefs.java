ac634b9f9a2a38974bf9ac5b0a2f31e16eb244f0
Peerasit <peerasitearth@outlook.com>
2023-10-22 12:04:31 +0700

    complete

---
+++ src/test/java/ku/shop/BuyStepdefs.java
@@ -0,0 +1,53 @@
+package ku.shop;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BuyStepdefs {
+
+    private ProductCatalog catalog;
+    private Order order;
+
+    private ArrayList<String> nameProduct = new ArrayList<>();
+
+    @Given("the store is ready to service customers")
+    public void the_store_is_ready_to_service_customers() {
+        catalog = new ProductCatalog();
+        order = new Order();
+    }
+
+    @Given("a product {string} with price {float} and stock of {int} exists")
+    public void a_product_exists(String name, double price, int stock) {
+        nameProduct.add(name);
+        catalog.addProduct(name, price, stock);
+    }
+
+    @When("I buy {string} with quantity {int}")
+    public void i_buy_with_quantity(String name, int quantity) {
+        Product prod = catalog.getProduct(name);
+        order.addItem(prod, quantity);
+    }
+
+    @Then("total should be {float}")
+    public void total_should_be(double total) {
+        assertEquals(total, order.getTotal());
+    }
+
+    @Then("total of stock is {int}")
+    public void total_stock_should_be(int stock) {
+        int totalStock = 0;
+        for (String name: nameProduct) {
+            Product prod = catalog.getProduct(name);
+            totalStock += prod.getStock();
+        }
+        assertEquals(stock, totalStock);
+    }
+//    นาย พีรสิษฐ์ พลอยอร่าม 6410451237
+}
+
---
+++ src/test/resources/features/buy.feature
@@ -0,0 +1,20 @@
+Feature: Buy products
+    As a customer
+    I want to buy products
+
+Background:
+    Given the store is ready to service customers
+    And a product "Bread" with price 20.50 and stock of 5 exists
+    And a product "Jam" with price 80.00 and stock of 10 exists
+    And a product "Butter" with price 50 and stock of 5 exists
+
+Scenario: Buy one product
+    When I buy "Bread" with quantity 2
+    Then total should be 41.00
+
+Scenario: Buy multiple products
+    When I buy "Bread" with quantity 2
+    And I buy "Jam" with quantity 1
+    And I buy "Butter" with quantity 3
+    Then total should be 271.00
+    Then total of stock is 14
